ad1b9f685c7e09491e9aa52b42fd5d5b33a5ce4b
ChiFang <hardman761110@gmail.com>
2017-07-29 13:08:15 +0800

    first draft

---
+++ RestaurantSys/RestaurantSys/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RestaurantSys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AdRotator = new System.Windows.Forms.PictureBox();
+            this.AdopenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.Adtimer = new System.Windows.Forms.Timer(this.components);
+            this.AdListBox = new System.Windows.Forms.ListBox();
+            this.AdIntervalText = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.SetButton = new System.Windows.Forms.Button();
+            this.OrderButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.AdRotator)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AdRotator
+            // 
+            this.AdRotator.Location = new System.Drawing.Point(12, 12);
+            this.AdRotator.Name = "AdRotator";
+            this.AdRotator.Size = new System.Drawing.Size(441, 340);
+            this.AdRotator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.AdRotator.TabIndex = 0;
+            this.AdRotator.TabStop = false;
+            this.AdRotator.Click += new System.EventHandler(this.AdRotator_Click);
+            // 
+            // AdopenFileDialog
+            // 
+            this.AdopenFileDialog.FileName = "openFileDialog1";
+            // 
+            // Adtimer
+            // 
+            this.Adtimer.Enabled = true;
+            this.Adtimer.Interval = 2000;
+            this.Adtimer.Tick += new System.EventHandler(this.Adtimer_Tick);
+            // 
+            // AdListBox
+            // 
+            this.AdListBox.FormattingEnabled = true;
+            this.AdListBox.ItemHeight = 12;
+            this.AdListBox.Location = new System.Drawing.Point(473, 12);
+            this.AdListBox.Name = "AdListBox";
+            this.AdListBox.Size = new System.Drawing.Size(120, 112);
+            this.AdListBox.TabIndex = 1;
+            // 
+            // AdIntervalText
+            // 
+            this.AdIntervalText.Location = new System.Drawing.Point(460, 201);
+            this.AdIntervalText.Name = "AdIntervalText";
+            this.AdIntervalText.Size = new System.Drawing.Size(56, 22);
+            this.AdIntervalText.TabIndex = 2;
+            this.AdIntervalText.Text = "2000";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(460, 141);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(541, 141);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.RemoveButton.TabIndex = 4;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // SetButton
+            // 
+            this.SetButton.Location = new System.Drawing.Point(522, 199);
+            this.SetButton.Name = "SetButton";
+            this.SetButton.Size = new System.Drawing.Size(75, 23);
+            this.SetButton.TabIndex = 5;
+            this.SetButton.Text = "Set (ms)";
+            this.SetButton.UseVisualStyleBackColor = true;
+            this.SetButton.Click += new System.EventHandler(this.SetButton_Click);
+            // 
+            // OrderButton
+            // 
+            this.OrderButton.Location = new System.Drawing.Point(160, 358);
+            this.OrderButton.Name = "OrderButton";
+            this.OrderButton.Size = new System.Drawing.Size(137, 29);
+            this.OrderButton.TabIndex = 6;
+            this.OrderButton.Text = "Order";
+            this.OrderButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 399);
+            this.Controls.Add(this.OrderButton);
+            this.Controls.Add(this.SetButton);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.AdIntervalText);
+            this.Controls.Add(this.AdListBox);
+            this.Controls.Add(this.AdRotator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.AdRotator)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox AdRotator;
+        private System.Windows.Forms.OpenFileDialog AdopenFileDialog;
+        private System.Windows.Forms.Timer Adtimer;
+        private System.Windows.Forms.ListBox AdListBox;
+        private System.Windows.Forms.TextBox AdIntervalText;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button SetButton;
+        private System.Windows.Forms.Button OrderButton;
+    }
+}
+
---
+++ RestaurantSys/RestaurantSys/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#region EmguCV_lib
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+#endregion
+
+namespace RestaurantSys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Global
+        {
+            public static bool bPlayingVideo = false;
+            public static Capture AdFrameGrabber;
+            public static int AdtimerIntervalBuffer = 0;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (AdopenFileDialog.ShowDialog() == DialogResult.OK)
+                AdListBox.Items.Add(AdopenFileDialog.FileName);
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            if (AdListBox.SelectedIndex != -1)
+                AdListBox.Items.RemoveAt(AdListBox.SelectedIndex);
+        }
+
+        private void SetButton_Click(object sender, EventArgs e)
+        {
+            Adtimer.Interval = int.Parse(AdIntervalText.Text);
+        }
+
+        private void Adtimer_Tick(object sender, EventArgs e)
+        {
+
+            if (AdListBox.Items.Count != 0)
+            {
+                if (!Global.bPlayingVideo)
+                {
+                    AdListBox.SelectedIndex = (AdListBox.SelectedIndex + 1) % AdListBox.Items.Count;
+                    AdRotator.ImageLocation = AdListBox.SelectedItem.ToString();
+
+                    int IsVedeo = AdRotator.ImageLocation.IndexOf(".avi");
+                    if (IsVedeo >= 0)
+                    {
+                        // 遇到影片 必須改變 timer1.Interval 為 1/fps 並且停止更新 AdListBox.SelectedIndex 直到影片播完
+
+
+                        Global.AdFrameGrabber = new Capture(AdRotator.ImageLocation);
+
+                        //Get the frame rate
+                        double rate = Global.AdFrameGrabber.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
+
+                        //Get the frame number
+                        double FrameNum = Global.AdFrameGrabber.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameCount);
+
+                        Global.AdtimerIntervalBuffer = Adtimer.Interval;
+                        int NewInterval = (int)(1000/rate);
+                        Adtimer.Interval = NewInterval;
+
+                        Global.bPlayingVideo = true;
+
+                    }
+                    // AdRotator.ImageLocation = "https://i0.wp.com/free.com.tw/blog/wp-content/uploads/2014/08/Placekitten480-g.jpg?resize=640%2C480&ssl=1";
+                    // AdRotator.ImageLocation = "C:\\Users\\user\\Desktop\\Out_just_track.avi";
+                }
+                else
+                {
+                    Image<Bgr, Byte> currentFrame;
+                    Emgu.CV.Mat frame = null;
+
+                    //read next frame if any
+                    frame = Global.AdFrameGrabber.QueryFrame();
+
+                    if (frame == null)
+                    {
+                        // 影片結束 回到輪播
+                        //Close the video file. Not required since called by destructor
+                        Global.AdFrameGrabber.Dispose();
+                        Adtimer.Interval = Global.AdtimerIntervalBuffer;
+                        Global.bPlayingVideo = false;
+                    }
+                    else
+                    {
+                        currentFrame = frame.ToImage<Bgr, byte>(false);
+                        AdRotator.Image = currentFrame.Bitmap;
+                    }
+                }
+
+
+            }
+        }
+
+        private void AdRotator_Click(object sender, EventArgs e)
+        {
+            if (AdListBox.Visible)
+            {
+                AdListBox.Visible = false;
+                AddButton.Visible = false;
+                RemoveButton.Visible = false;
+                SetButton.Visible = false;
+                AdIntervalText.Visible = false;
+            }
+            else
+            {
+                AdListBox.Visible = true;
+                AddButton.Visible = true;
+                RemoveButton.Visible = true;
+                SetButton.Visible = true;
+                AdIntervalText.Visible = true;
+            }
+        }
+    }
+}
